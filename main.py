745952b55dd1034b1bfd16b0907ad63bbb21c99e
ignacy <ignacypr@gmail.com>
2022-06-04 02:25:02 +0200

    rundy działają chyba tez jak przeliczyłem recznie dla małego inputu to działa

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ d = 6
 
 
 def setDimension(dimension):
+    if dimension<3:
+        raise TypeError("Minimum dimension 3")
     global d
     d = dimension
     print("Dimension set to", d)
@@ -57,14 +59,13 @@ def L(byteInput):
     return bitarray(C + D).tobytes()
 
 
-#print(L(b'\x66'))
+# print(L(b'\x66'))
 
 
 def pi(byteInput):
     if type(byteInput) != bytes:
         raise TypeError
-    if len(byteInput) * 2 != 2 ** d:
-        raise Exception("input dimension must be " + str(d))
+
     input = byteToTab(byteInput)
     res = []
     for i in range(0, int(len(input) / 4)):
@@ -75,49 +76,71 @@ def pi(byteInput):
     return bitarray(res).tobytes()
 
 
-
-
 def permPrim(byteInput):
     if type(byteInput) != bytes:
         raise TypeError
-    if len(byteInput) * 2 != 2 ** d:
-        raise Exception("input dimension must be " + str(d))
     input = byteToTab(byteInput)
     res = [0] * len(input)
 
     for i in range(int(len(input) / 2)):
         res[i] = input[2 * i]
-        res[i + 2 ** (d +1)] = input[2 * i + 1]
+        res[i + 2 ** (d -1)] = input[2 * i + 1]
     return bitarray(res).tobytes()
+
 
 def fi(byteInput):
     if type(byteInput) != bytes:
         raise TypeError
-    if len(byteInput) * 2 != 2 ** d:
-        raise Exception("input dimension must be " + str(d))
     input = byteToTab(byteInput)
-    halfLength = int(len(input)/2)
-    res = [0]*len(input)
+    halfLength = int(len(input) / 2)
+    res = [0] * len(input)
 
-    for i in range(0,halfLength,2):
+    for i in range(0, halfLength, 2):
         res[i] = input[i]
-        res[i+1] = input[i+1]
-        res[i+halfLength] = input[i+1+halfLength]
-        res[i+1+halfLength] = input[i+halfLength]
+        res[i + 1] = input[i + 1]
+        res[i + halfLength] = input[i + 1 + halfLength]
+        res[i + 1 + halfLength] = input[i + halfLength]
     return bitarray(res).tobytes()
+
+
 def permutation(byteInput):
     if type(byteInput) != bytes:
         raise TypeError
-    if len(byteInput) * 2 != 2 ** d:
-        #print(len(byteInput) * 2,2**d)
-        raise Exception("input dimension must be " + str(d))
     return fi(permPrim(pi(byteInput)))
 
 
-setDimension(3)
-print(d)
-x = randbytes(2**(d-1))
-print(x)
-print(x.hex())
-print(permutation(x).hex())
+def S(index, bits):
+    if (type(index) != int and (index == 0 or index == 1)) or (type(bits) != int and -1 < bits < 16):
+        raise TypeError
+    return \
+        [[9, 0, 4, 11, 13, 12, 3, 15, 1, 10, 2, 6, 7, 5, 8, 14],
+         [3, 12, 6, 13, 5, 7, 1, 9, 15, 2, 0, 4, 11, 10, 14, 8]][index][bits]
 
+def round(byteInput):
+    if type(byteInput) != bytes:
+        raise TypeError
+    if len(byteInput) != 2 ** (d-3):
+        raise Exception("input dimension must be " + str(d))
+    input = byteToTab(byteInput)
+    temp =[]
+    for i in range(0,2**d,8):
+        temp.append((S(0,input[i]*8+input[i+1]*4+input[i+2]*2+input[i+3])*16+S(1, input[i+4] * 8 + input[i + 5] * 4 + input[i + 6] * 2 + input[i + 7])).to_bytes(1,"big"))
+    res = bytes()
+    for i in temp:
+        res+=L(i)
+    return permutation(res)
+
+
+
+
+
+
+
+setDimension(3)
+#print(d)
+x = b'\x66'#randbytes(2 ** (d - 3))
+#print(x)
+#print(x.hex())
+print(permutation(b'\xd7').hex())
+print(round(x).hex())
+print(L(b'\31'))
