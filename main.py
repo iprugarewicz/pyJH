0246e0f2dca2d3a09e4e93fe5e714b4a7d8137af
ignacy <01168951@pw.edu.pl>
2022-06-05 14:36:34 +0200

    .

---
+++ main.py
@@ -0,0 +1,7 @@
+import pyJH
+print(pyJH.JH224(""))
+print(pyJH.JH256(""))
+print(pyJH.JH384(""))
+print(pyJH.JH512(""))
+print(pyJH.JH256("The quick bron fox jumps over the lazy dog"))
+print(pyJH.JH156("The quick bron fox jumps over the lazy dog."))
